c8f3e36f61c5de3da57d0709f0f7dc590cb62858
trisimix <josh@b-labs.net>
2022-10-20 17:05:24 +0000

    Code changes from 2022-10-20_17:05:24

--- index.js
+++ index.js
@@ -4,11 +4,19 @@
 // Full Mineflayer docs: https://mineflayer.prismarine.js.org/#/
 
 const mineflayer = require('mineflayer')
+const { pathfinder, Movements } = require('mineflayer-pathfinder')
+const { GoalNear, GoalBlock, GoalGetToBlock, GoalLookAtBlock, GoalXZ, GoalY, GoalInvert, GoalFollow } = require('mineflayer-pathfinder').goals
+const { Vec3 } = require('vec3');
 
 /**
  * @param {mineflayer.Bot} bot - The Mineflayer bot
  */
 function configureBot(bot) {
+
+  // Configure the bot to have access to Mineflayer modules/plugins
+  bot.loadPlugin(pathfinder)
+  const mcData = require('minecraft-data')(bot.version)
+  const defaultMove = new Movements(bot, mcData)
 
   bot.on('chat', async (username, message) => {
     try {
